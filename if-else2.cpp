9d76c8ede7739a44b28bd5afc79c46c1e2c88dcd
atharvdalvi2003 <atharvdalvi25@gmail.com>
2024-06-24 15:30:35 +0530

    Add files via upload

---
+++ if-else2.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+	int a=101;
+	int b=200;
+	if(a==100){
+		printf("1st if a=100");
+		
+	}
+	else{
+		printf("\n 1st if block condition is false\n");
+	}
+	if(b==200){
+		printf("value of b = 200");
+	}
+	else{
+		printf("\n 2nd if block condition is false");
+    }
+
+}
+
